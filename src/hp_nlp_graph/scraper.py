703915f131bd006c3e15c35a02ef48a2a78f6df3
nikhil-ravi <nr337@cornell.edu>
2023-10-26 12:49:16 -0400

    Improvements to the character scraper to support scraping from book 3 onwards

--- src/hp_nlp_graph/scraper.py
+++ src/hp_nlp_graph/scraper.py
@@ -4,67 +4,9 @@ import requests
 from bs4 import BeautifulSoup
 
 BASE_URL = "https://harrypotter.fandom.com"
-CHARACTERS_LIST_URL = (
+CHARACTERS_LIST_URL_EXAMPLE = (
     BASE_URL + "/wiki/Harry_Potter_and_the_Philosopher%27s_Stone_(character_index)"
 )
-
-
-@dataclass
-class Character:
-    """Represents a Harry Potter character with associated attributes."""
-
-    title: str
-    href: str
-    aliases: list = None
-    loyalties: list = None
-    famliy_relations: list = None
-    blood_status: str = None
-    nationality: str = None
-    species: str = None
-    house: str = None
-    gender: str = None
-
-    def set_attr(self, attr: str, value):
-        """Sets the value of an attribute.
-
-        Args:
-            attr (str): The name of the attribute to set.
-            value (_type_): The value to set the attribute to.
-        """
-        setattr(self, attr, value)
-
-
-@dataclass
-class Chapter:
-    """Represents a chapter in a Harry Potter book and the characters mentioned in it."""
-
-    chapter: int
-    characters: list[Character]
-
-
-def get_characters_by_chapter() -> list[Chapter]:
-    """Gets a list of chapters and the characters mentioned in them.
-
-    Returns:
-        list[Chapter]: A list of chapters and the characters mentioned in them.
-    """
-    res = requests.get(CHARACTERS_LIST_URL).text
-    soup = BeautifulSoup(res, "html.parser")
-    div = soup.find_all("div", class_="mw-parser-output")
-    tables = div[0].find_all("table", class_="article-table")
-    chapters = []
-    for chapter, table in enumerate(tables):
-        list_of_characters = []
-        for tr in table.find_all("tr"):
-            td = tr.find("td")
-            character = td.find("a")
-            if not character or not character.get("title"):
-                continue
-            list_of_characters.append(
-                Character(title=character.get("title"), href=character.get("href"))
-            )
-        chapters.append(Chapter(chapter=chapter + 1, characters=list_of_characters))
-    return chapters
 
 
 def get_aliases(soup: BeautifulSoup) -> list[str]:
@@ -155,17 +97,107 @@ ATTRIBUTES_TO_SCRAPE = [
 ]
 
 
-def add_character_info(character: Character) -> None:
-    """Adds additional information to a character.
+@dataclass
+class Character:
+    """Represents a Harry Potter character with associated attributes."""
 
-    Args:
-        character (Character): The character to add information to.
+    title: str
+    href: str
+    aliases: list = None
+    loyalties: list = None
+    famliy_relations: list = None
+    blood_status: str = None
+    nationality: str = None
+    species: str = None
+    house: str = None
+    gender: str = None
+
+    def set_attr(self, attr: str, value):
+        """Sets the value of an attribute.
+
+        Args:
+            attr (str): The name of the attribute to set.
+            value (_type_): The value to set the attribute to.
+        """
+        setattr(self, attr, value)
+
+    def enrich(self) -> None:
+        """Adds additional information to a character.
+
+        Args:
+            character (Character): The character to add information to.
+        """
+        res = requests.get(BASE_URL + self.href).text
+        soup = BeautifulSoup(res, "html.parser")
+        for attr_name, scraper_func in ATTRIBUTES_TO_SCRAPE:
+            try:
+                value = scraper_func(soup)
+                self.set_attr(attr_name, value)
+            except Exception as e:
+                pass
+
+
+@dataclass
+class Chapter:
+    """Represents a chapter in a Harry Potter book and the characters mentioned in it."""
+
+    chapter: int
+    characters: list[Character]
+
+
+def get_characters_by_chapter(url: str = CHARACTERS_LIST_URL_EXAMPLE) -> list[Chapter]:
+    """Gets a list of chapters and the characters mentioned in them.
+
+    Returns:
+        list[Chapter]: A list of chapters and the characters mentioned in them.
     """
-    res = requests.get(BASE_URL + character.href).text
+    res = requests.get(url).text
     soup = BeautifulSoup(res, "html.parser")
-    for attr_name, scraper_func in ATTRIBUTES_TO_SCRAPE:
+    div = soup.find_all("div", class_="mw-parser-output")
+    chapter_sections = div[0].find_all("h2")
+    chapters = []
+    chapter_idx = 1
+    for chapter_section in chapter_sections:
         try:
-            value = scraper_func(soup)
-            character.set_attr(attr_name, value)
-        except Exception as e:
-            pass
+            chapter_name = chapter_section.find("span", class_="mw-headline").text
+            if not (
+                chapter_name.startswith("Chapter")
+                or chapter_name.startswith("Epilogue")
+            ):
+                continue
+        except AttributeError:
+            continue
+
+        next_sibling = chapter_section.find_next_sibling()
+        if next_sibling.name == "ul":
+            list_of_characters = []
+            for li in next_sibling.find_all("li"):
+                character = li.find("a")
+                if not character or not character.get("title"):
+                    continue
+                list_of_characters.append(
+                    Character(title=character.get("title"), href=character.get("href"))
+                )
+            chapters.append(Chapter(chapter=chapter_idx, characters=list_of_characters))
+            chapter_idx += 1
+        elif next_sibling.name == "table":
+            list_of_characters = []
+            for tr in next_sibling.find_all("tr"):
+                td = tr.find("td")
+                character = td.find("a")
+                if not character or not character.get("title"):
+                    continue
+                list_of_characters.append(
+                    Character(title=character.get("title"), href=character.get("href"))
+                )
+            chapters.append(Chapter(chapter=chapter_idx, characters=list_of_characters))
+            chapter_idx += 1
+        elif next_sibling.name == "dl":  # For chapters with no new characters
+            chapters.append(Chapter(chapter=chapter_idx, characters=[]))
+            chapter_idx += 1
+        elif (
+            next_sibling.name == "p"
+        ):  # For some chapters (Book 6: Chapter 25) with no new characters
+            chapters.append(Chapter(chapter=chapter_idx, characters=[]))
+            chapter_idx += 1
+    return chapters
